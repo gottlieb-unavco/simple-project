03814a61bdccace2be12ca59b9680642a1f49609
Adam Clark <adam@iris.washington.edu>
2021-07-12 15:38:13 -0700

    Show recently archived messages on the page

--- django/kafka_test/views.py
+++ django/kafka_test/views.py
@@ -4,24 +4,33 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from kafka_test.kafka.producer import produce_example_message
 from kafka_test.models import ExampleValue
-import uuid
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 
 class TestForm(forms.ModelForm):
+    """
+    Form for the ExampleValue
+    """
     class Meta:
         model = ExampleValue
         fields = ['value']
 
 
 class IndexView(FormView):
+    """
+    It's a form with one field, the value to send
+    """
     template_name = 'kafka_test/index.html'
     form_class = TestForm
+    # When done, redirect back to this page
     success_url = reverse_lazy('kafka-test')
 
     def form_valid(self, form):
+        """
+        The form itself is good, try sending it as a message
+        """
         try:
             value = form.cleaned_data.get('value')
             produce_example_message(value)
@@ -30,6 +39,7 @@ class IndexView(FormView):
                 messages.SUCCESS,
                 "Added to queue: '%s'" % value,
             )
+            LOGGER.debug("Added to queue: %s", value)
         except Exception as e:
             LOGGER.error(e, exc_info=True)
             messages.add_message(
@@ -37,4 +47,15 @@ class IndexView(FormView):
                 messages.ERROR,
                 "Uh oh! %s" % e,
             )
+        # Those operations set a message in the response
+        # but the response itself is always the form looking ok
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        """
+        Add data to the template context
+        """
+        context = super().get_context_data(**kwargs)
+        # Include the last few processed items
+        context['recent'] = ExampleValue.objects.order_by('-created_date')[:20]
+        return context
