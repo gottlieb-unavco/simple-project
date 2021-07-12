f89bae6544f284590760027408d7db8d9162b4e6
Adam Clark <adam@iris.washington.edu>
2021-07-12 15:25:55 -0700

    Proxy static files with nginx. Also moves the gunicorn server to port 80 (mapped to 8888) and the base server to 8080.

--- django/www/settings.py
+++ django/www/settings.py
@@ -118,11 +118,15 @@ USE_L10N = False
 
 USE_TZ = False
 
+DATE_FORMAT = 'Y-m-d'
+DATETIME_FORMAT = 'Y-m-d H:i:s'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '..' / 'django-static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -145,10 +149,11 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
+            'level': 'DEBUG',
         },
     },
     'root': {
         'handlers': ['console'],
-        'level': 'INFO',
+        'level': 'DEBUG',
     },
 }
