7ee46295a3496b1200fec5586f95401300775cc6
Adam Clark <adam@iris.washington.edu>
2021-07-13 16:16:00 -0700

    Both standard html and websockets should work now

--- django/kafka_test/models.py
+++ django/kafka_test/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from datetime import timedelta
 
 
 def random_value():
@@ -27,3 +28,10 @@ class ExampleValue(models.Model):
         """
         if self.timestamp and self.created_date:
             return self.created_date - self.timestamp
+
+    def delay_ms(self):
+        """
+        Calculate the delay from when the message was sent to when
+        it was saved to the db
+        """
+        return self.delay() / timedelta(milliseconds=1)
