107b7b7cc957d19829edef6ed4aa5eab2d813a97
Adam Clark <adam@iris.washington.edu>
2021-07-13 11:57:47 -0700

    Django application for round-tripping messages over kafka.

---
+++ django/kafka_test/management/commands/run_kafka_consumer.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from kafka_test.kafka.consumer import ExampleConsumer
+from logging import getLogger
+
+LOGGER = getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Run the consumer'
+
+    def handle(self, **options):
+        consumer = ExampleConsumer()
+        consumer.run()
+
