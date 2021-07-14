a2559e42ea7a174017fedfd74e21e09baa5f38ef
Adam Clark <adam@iris.washington.edu>
2021-07-13 23:03:48 -0700

    Rebuilt the socket interface around Promises Code documentation Made the "status" area on the websocket example page a socket log

--- nginx/html/ws-example/ws-example.js
+++ nginx/html/ws-example/ws-example.js
@@ -1,56 +1,108 @@
-
 function getSocket(onmessage) {  
   var state = 'init';
   var localhost = "" + location.host;
   var wsURL = 'ws://' + localhost + '/example/ws/';
-  var socket = null;
+  // Promise that returns a socketPromise socket
+  var socketPromise = null;
+
+  // Base retry timeout
+  var retryBaseTimeout = 4000;
+  // How much to back off each repeated retry
+  var retryBackoff = 2;
+  // How many retries we've done
+  var retries = 0;
 
   /**
-   * Create a new websocket
+   * Create a promise of a new socket
+   * @returns Promise returning the socket when it becomes available
    */
-  function createSocket() {
+  function createSocketPromise() {
     console.log(wsURL);
-    socket = new WebSocket(wsURL);
+    socketPromise = $.Deferred();
+    var socket = new WebSocket(wsURL);
     socket.onopen = function() {
-      state = 'open';
+      socketPromise.resolve(socket);
+      retries = 0;
     };
     socket.onmessage = onmessage;
     socket.onerror = function() {
       console.log("Error");
-      state = 'error';
+      socketPromise.reject();
     }
     socket.onclose = function() {
       console.log("Closing");
       var wasError = (state == 'error');
-      state = 'closed';
-      socket = null;
+      socketPromise.reject();
+      socketPromise = null;
       if (!wasError) {
-        setTimeout(createSocket, 15000);
+        var timeout = (retries ^ retryBackoff) * retryBaseTimeout;
+        console.log("Will retry socket in " + timeout + "ms");
+        setTimeout(createSocketPromise, timeout);
+        // Increment retries if we come here again
+        retries++;
       }
     };
-    return socket;
+    return socketPromise;
   }
 
-  function send(message) {
-    if (!socket || socket.readyState == socket.CLOSED) {
-      console.log('Reloading socket');
-      createSocket();
+  /**
+   * Return a promise of a working socket. Mainly reconnects if a stale
+   * socket is found.
+   * @returns Promise returning the socket when it becomes available
+   */
+  function awaitSocket() {
+    // No working promise, create a new one
+    if (!socketPromise) {
+      return createSocketPromise();
     }
-    console.log('Send message');
-    socket.send(JSON.stringify(message));
+    return socketPromise.then(
+      // Got a socket, but it may be stale
+      function(socket) {
+        if (!socket || socket.socketPromiseState == socket.CLOSED) {
+          console.log('Reloading socket');
+          return createSocket();
+        }
+        return socket;         
+      },
+      // We could retry on failure, but the socket onclose does this?
+      // createSocketPromise,
+    )
   }
 
-  createSocket();
+  /**
+   * Send a message to the socket
+   * @param {object} message JSON data for the message
+   */
+  function send(message) {
+    awaitSocket().then(function(socket) {
+      socket.send(JSON.stringify(message));
+    });
+  }
+
+  // Start up a connection on initialization
+  createSocketPromise();
+
+  // Just return the send API
   return {
-    createSocket: createSocket,
     send: send,
   };
 }
 
+/**
+ * Return the difference between 2 times (Dates or Strings)
+ * @param {Date, String} t1 
+ * @param {Date, String} t2 
+ * @returns difference in ms
+ */
 function timeDiff(t1, t2) {
   return (new Date(t2) - new Date(t1));
 }
 
+/**
+ * Create a list item for the "latest" list
+ * @param {object} item an item record returned from the 'list' API
+ * @returns jQuery
+ */
 function createItem(item) {
   var delay = timeDiff(item.timestamp, item.created_date);
   var $delay = $('<span class="delay">').text("" + delay + "ms");
@@ -64,27 +116,60 @@ function createItem(item) {
     $delay,
   );
 }
+
+
+var people = [
+  'Homer', 'Marge', 'Bart', 'Lisa', 'Maggie', 'Moe', 'Barney', 
+  'Carl', 'Lenny', 'Mr. Burns', 'Bumblebee Man', 'McBain',
+];
+var locations = [
+  '642 Evergreen Terrace', "Moe's Bar", 'Springfield Elementary',
+  'Springfield Nuclear Power Plant', 'Capital City',
+];
+function getRandomItem(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+function makeMessageText() {
+  return (
+    "" + getRandomItem(people) +
+    " calling " + getRandomItem(people) +
+    " from " + getRandomItem(locations)
+  );
+}
+
+
 /**
  * Attach the socket API on top of the page elements
+ * 
+ * @param {jQuery} $form container for the form (buttons)
+ * @param {jQuery} $status container for the websocket status
+ * @param {jQuery} $archive container for the archive data
  */
- function attachSocket($form, $status, $latest) {
-  var $connStatus = $('<div class="conn-status" />');
-  $status.append($connStatus);
-  var $responseStatus = $('<div class="resp-status" />');
-  $status.append($responseStatus);
+ function attachSocket($form, $status, $archive) {
+  var $socketStatus = $('<div class="socket-status" />');
+  $status.append($socketStatus);
 
+  /**
+   * Log a line of status from the websocket
+   * @param {String} desc 
+   * @param {object} data 
+   */
   function addStatusLine(desc, data) {
     var $statusLine = (
       $('<div class="status-line">')
       .addClass(desc)
       .text(JSON.stringify(data))
     );
-    $('.status-line', $responseStatus).slice(0, -9).remove();
-    $responseStatus.append($statusLine);
+    $('.status-line', $socketStatus).slice(0, -9).remove();
+    $socketStatus.append($statusLine);
     // Scroll to the new line
     $statusLine.get(0).scrollIntoView(false);
   }
 
+  /**
+   * Handle a message from the socket
+   * @param {object} message 
+   */
   function onmessage(message) {
     var response = JSON.parse(message.data);
     console.log(response);
@@ -92,20 +177,23 @@ function createItem(item) {
     if (response.status == 'ok') {
       // Text result
       if (response.result) {
-        // Refresh the list
+        // Trigger a list refresh shortly after we run a command
         setTimeout(() => $refresh.click(), 500);
       }
-      // If row items returned, show them in the list
+      // Archive items go in the list
       if (response.items) {
-        $latest.empty().append(
+        $archive.empty().append(
           response.items.map(createItem)
         );
       }
     }
   }
   var ws = getSocket(onmessage);
-  var messageIdx = 1;
 
+  /**
+   * Send a message on the socket
+   * @param {object} message 
+   */
   function sendMessage(message) {
     ws.send(message);
     addStatusLine('out', message);
@@ -113,10 +201,9 @@ function createItem(item) {
 
   var $test = $('<button type="button">').text('Send a test message');
   $test.click(function() {
-    var value = 'Message ' + messageIdx++;
     sendMessage({
       action: 'send',
-      value: value,
+      value: makeMessageText(),
     });
   });
   var $refresh = $('<button type="button">').text('Update latest');
@@ -126,5 +213,9 @@ function createItem(item) {
     });
   });
 
-  $form.after($test, $refresh, $status);
+  $form.append($test, $refresh);
+
+  sendMessage({
+    action: 'list',
+  });
 }
