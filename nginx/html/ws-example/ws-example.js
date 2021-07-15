0f8ce8c2277afddbe6391e195dae085a31f22ede
Adam Clark <adam@iris.washington.edu>
2021-07-15 12:23:28 -0700

    Update WebSocket example to use a data identifier

--- nginx/html/ws-example/ws-example.js
+++ nginx/html/ws-example/ws-example.js
@@ -134,6 +134,21 @@ function makeMessageText() {
   );
 }
 
+/**
+ * UUID4 quick and dirty version
+ * @see https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523
+ * @returns String
+ */
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function makeDataId() {
+  return "example:ws/" + uuidv4();
+}
 
 /**
  * Attach the socket API on top of the page elements
@@ -200,7 +215,7 @@ function makeMessageText() {
   $test.click(function() {
     sendMessage({
       action: 'send',
-      value: makeMessageText(),
+      value: makeDataId(),
     });
   });
   var $refresh = $('<button type="button">').text('Refresh list');
