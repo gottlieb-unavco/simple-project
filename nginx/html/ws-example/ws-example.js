6f704fb0789d5aa68b76cc32c3448a7a17a9395b
Adam Clark <adam@iris.washington.edu>
2021-07-16 18:01:47 -0700

    Added data identifier to the example schema

--- nginx/html/ws-example/ws-example.js
+++ nginx/html/ws-example/ws-example.js
@@ -126,6 +126,7 @@ function createItem(item) {
   ));
   return $('<li>').append(
     $('<span class="value">').text('' + item.value),
+    $('<span class="data_id">').text('' + item.data_id),
     $('<span class="timestamp">').text('' + item.timestamp),
     $delay,
   );
@@ -153,21 +154,22 @@ function debounce(func, wait, immediate) {
 }
 
 
-/**
- * UUID4 quick and dirty version
- * @see https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523
- * @returns String
- */
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
-function makeDataId() {
-  return "example:ws/" + uuidv4();
-}
+var people = [
+  'Homer', 'Marge', 'Bart', 'Lisa', 'Maggie', 'Moe', 'Barney', 
+  'Carl', 'Lenny', 'Mr. Burns', 'Bumblebee Man', 'McBain',
+];
+var locations = [
+  '642 Evergreen Terrace', "Moe's Bar", 'Springfield Elementary',
+  'Springfield Nuclear Power Plant', 'Capital City',
+];
+function getRandomItem(list) {
+  return list[Math.floor(Math.random() * list.length)];
+function makeMessageText() {
+  return (
+    "" + getRandomItem(people) +
+    " texts " + getRandomItem(people) +
+    " from " + getRandomItem(locations)
+  );
 
 /**
  * Attach the socket API on top of the page elements
@@ -233,7 +235,7 @@ function makeDataId() {
   function sendOneMessage() {
     sendToSocket({
       action: 'send',
-      value: makeDataId(),
+      value: makeMessageText(),
     });
   }
 
